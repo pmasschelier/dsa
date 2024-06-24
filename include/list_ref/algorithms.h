dbf98d2415ef996caaad92d5dc7f07e50c9ef331
PM <pmasschelier@mailo.com>
2024-06-24 20:43:56 +0200

    list transform implementation

--- include/list_ref/algorithms.h
+++ include/list_ref/algorithms.h
@@ -12,4 +12,20 @@
  */
 node_ref_t* find_in_list(list_ref_t* list, void* x);
 
+typedef void (*transform_list_fn_t)(void*);
+
+/**
+ * @brief Call the transform function on every element owned by the list
+ *
+ * @param list pointer to the list
+ * @param f function to apply to the elements
+ */
+void transform_list(list_ref_t* list, transform_list_fn_t f);
+
+typedef BOOL (*list_filter_fn_t)(void*);
+
+// void filter_list(list_ref_t* list,
+// 				 list_ref_t** others,
+// 				 list_filter_fn_t* filter);
+
 #endif	// !LISTREFALGLORITHMS_H
--- src/list_ref/algorithms.c
+++ src/list_ref/algorithms.c
@@ -1,5 +1,6 @@
 #include "list_ref/algorithms.h"
 #include <string.h>
+#include "list_ref/list_ref.h"
 
 node_ref_t* find_in_list(list_ref_t* list, void* x) {
 	node_ref_t* node = list->begin;
@@ -9,4 +10,48 @@ node_ref_t* find_in_list(list_ref_t* list, void* x) {
 		node = node->next;
 	}
 	return NULL;
+}
+
+static void transform_list_node(node_ref_t* node, void (*f)(void*)) {
+	f(node->p);
+	if (node->next)
+		transform_list_node(node->next, f);
+}
+
+void transform_list(list_ref_t* list, void (*f)(void*)) {
+	transform_list_node(list->begin, f);
+}
+
+static void filter_list_extract(list_ref_t* list,
+								list_ref_t* others,
+								BOOL (*filter)(void*),
+								node_ref_t* node) {
+	node_ref_t* next = node->next;
+	if (!filter(node->p)) {
+		extract_list(list, node);
+		insert_list_node(others, list->end, node);
+	}
+
+	if (next)
+		filter_list_extract(list, others, filter, next);
+}
+
+static void filter_list_remove(list_ref_t* list,
+							   BOOL (*filter)(void*),
+							   node_ref_t* node) {
+	node_ref_t* next = node->next;
+	if (!filter(node->p))
+		remove_list(list, node, NULL);
+
+	if (next)
+		filter_list_remove(list, filter, next);
+}
+
+void filter_list(list_ref_t* list, list_ref_t** others, BOOL (*filter)(void*)) {
+	if (others) {
+		*others = create_list(list->size);
+		filter_list_extract(list, *others, filter, list->begin);
+	} else {
+		filter_list_remove(list, filter, list->begin);
+	}
 }
