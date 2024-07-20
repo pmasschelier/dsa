28cb9592d23ddeb78fa11a87100f4e2c9cf5c9ec
PM <pmasschelier@mailo.com>
2024-07-20 15:56:20 +0200

    Test: graph_list shortest paths algorithms

--- tests/graph_list/graph_list_bellman_unit_weights.c
+++ tests/graph_list/graph_list_bellman_unit_weights.c
@@ -1,22 +1,22 @@
 #include <assert.h>
 #include <graph/graph_list.h>
-#include <stdio.h>
 #include <weight_type.h>
+#include "config.h"
 
 #define EDGE_COUNT 6
-#define NODE_COUNT 4
+#define NODE_COUNT 6
 
 const unsigned int edges[EDGE_COUNT][2] = {
 	{0, 1},
 	{0, 2},
 	{1, 2},
-	{2, 0},
 	{2, 3},
-	{3, 3}
+	{3, 4},
+	{3, 5}
 };
 
-const graph_weight_t expected[NODE_COUNT] = {2, 0, 1, 2};
-const int expected_fathers[NODE_COUNT] = {2, -1, 1, 2};
+const graph_weight_t expected[NODE_COUNT] = {GRAPH_WEIGHT_INF, 0, 1, 2, 3, 3};
+const int expected_fathers[NODE_COUNT] = {-1, -1, 1, 2, 3, 3};
 
 graph_weight_t distance[NODE_COUNT];
 int father[NODE_COUNT];
@@ -25,11 +25,11 @@ int main(void) {
 	graph_list_t* g = create_graph_list(NODE_COUNT, FALSE);
 	for (int i = 0; i < EDGE_COUNT; i++)
 		graph_list_set_edge(g, edges[i][0], edges[i][1], TRUE, 0, FALSE);
-	graph_list_dijkstra(g, 1, distance, father);
-	for (unsigned i = 0; i < NODE_COUNT; i++) {
-		printf("%lld %d\n", distance[i], father[i]);
-		/* assert(expected[i] == distance[i]); */
-		/* assert(expected_fathers[i] == father[i]); */
+	assert(0 == graph_list_bellman(g, 1, distance, father));
+
+	for (int i = 0; i < NODE_COUNT; i++) {
+		assert(expected[i] == distance[i]);
+		assert(expected_fathers[i] == father[i]);
 	}
 	free_graph_list(g);
 	return 0;
---
+++ tests/graph_list/graph_list_outdegree.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include <graph/graph_list.h>
+
+#define EDGE_COUNT 6
+#define NODE_COUNT 4
+
+const unsigned int edges[EDGE_COUNT][2] = {
+	{1, 0},
+	{1, 2},
+	{1, 3},
+	{2, 1},
+	{2, 3},
+	{3, 3}
+};
+
+int tab[NODE_COUNT], father[NODE_COUNT];
+
+int main(void) {
+	graph_list_t* g = create_graph_list(NODE_COUNT, FALSE);
+	for (int i = 0; i < EDGE_COUNT; i++)
+		graph_list_set_edge(g, edges[i][0], edges[i][1], TRUE, 0, FALSE);
+	unsigned int degree = graph_list_outdegree(g, 1);
+	assert(degree == 3);
+	free_graph_list(g);
+	return 0;
+}
